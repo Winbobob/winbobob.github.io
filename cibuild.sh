dbb460bc266287340464c56e9cc2ae4afaedf949
Fernando Moreira <nandomoreira.me@gmail.com>
2015-11-18 16:55:43 -0200

    fix Gemfile and .travis.yml :ok_hand:

--- cibuild.sh
+++ cibuild.sh
@@ -2,9 +2,10 @@
 set -e # halt script on error
 
 echo 'Testing travis...'
+bundle install
 bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproof ./_site --disable-external
+bundle exec htmlproof ./_site --disable-external --only-4xx --check-favicon --check-html
 
 # config
 echo "Git config"
