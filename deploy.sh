a3997f5e033aa51daf7b368177472797ee4ff27a
Fernando Moreira <nandomoreira.me@gmail.com>
2015-11-19 01:28:00 -0200

    test travis..

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-echo 'Testing travis...'
-# bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproof ${HTML_FOLDER} --disable-external
+# bundle exec travis-lint
+# bundle exec htmlproof ${HTML_FOLDER} --disable-external
 
 # config
-echo "Git config"
 git config --global user.email "nandomoreira.me@gmail.com"
 git config --global user.name "Fernando Moreira"
 
 # deploy
-echo "Deploy"
 git init
 git add --all
 git commit -m "Deploy to GitHub Pages"
