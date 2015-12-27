bec8a40a31d710e278ce0697eddb1826ce48f4b7
Fernando Moreira <nandomoreira.me@gmail.com>
2015-12-26 22:30:19 -0200

    htmlproof --href-ignore

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e # halt script on error
 echo 'Testing travis...'
 bundle exec travis-lint
 bundle exec jekyll build
-bundle exec htmlproof ./_site --href-ignore "#" --disable-external
+bundle exec htmlproof ./_site --href-ignore "#","#!" --disable-external
 
 cd ${HTML_FOLDER}
 
