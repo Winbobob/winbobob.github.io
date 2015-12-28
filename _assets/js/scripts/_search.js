afdd88a9191037e116d3d57b31c940fe1c0d61da
Fernando Moreira <nandomoreira.me@gmail.com>
2015-12-27 23:58:32 -0200

    Fazendo um sistema de busca simples no Jekyll usando AngularJS

--- _assets/js/scripts/_search.js
+++ _assets/js/scripts/_search.js
@@ -8,14 +8,13 @@ app.config([
 
 app.controller('searchController', ['$scope', '$http', function($scope, $http){
   $scope.pesquisar = function(q) {
+    $scope.posts = [];
     if (q != "" && q != undefined && q.length >= 1) {
       $http.get('/search.json', { "data" : q }).success(function(data) {
         $scope.posts = data;
       }).error(function(data) {
         console.error('error...');
       });
-    } else {
-      $scope.posts = [];
     }
   };
 }]);
